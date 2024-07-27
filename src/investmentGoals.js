21a265b9ed758ae23ee5c6b9253d1f54c83d9a94
frankMarcelino <frank.silva.ti@gmail.com>
2024-07-27 13:33:37 -0300

    finalizando logica

--- src/investmentGoals.js
+++ src/investmentGoals.js
@@ -1,15 +1,47 @@
 function convertToMontlyReturnRate(yearlyReturnRate) {
-  return yearlyReturnRate ** (1/12)
+  return yearlyReturnRate ** (1 / 12);
 }
 
-
 function generateReturnArrays(
-  startingAmount = 0, 
-  timeHorizon = 0 , 
-  timePeriod = "monthly",
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
   monthlyContribution = 0,
-  returnRate  = 0,
-  returnTimeFrame = "monthly"
+  returnRate = 0,
+  returnTimeFrame = 'monthly',
 ) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      'Investimento inicial e prazo devem ser informados com valores positivos.',
+    );
+  }
 
+  const finalReturnRate =
+    returnTimeFrame === 'monthly'
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    ivestedAmount: startingAmount,
+    interrestReturn: 0,
+    totalInterestReturn: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnArray = [referenceInvestmentObject,];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+    const interestReturns = returnArray[timeReference - 1].totalAmount * finalReturnRate;
+    const ivestedAmount = startingAmount + monthlyContribution * timeReference;
+  }
 }
