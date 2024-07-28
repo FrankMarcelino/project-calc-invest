2fdb6eb4538b42bec8be203fb3d880f22953658c
frankMarcelino <frank.silva.ti@gmail.com>
2024-07-27 22:35:00 -0300

    recuperei os arquivos que tinha perdido

---
+++ src/investmentGoals.js
@@ -0,0 +1,57 @@
+function convertToMontlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+};
+
+export function generateReturnArrays(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = 'monthly',
+) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      'Investimento inicial e prazo devem ser informados com valores positivos.',
+    );
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === 'monthly'
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interrestReturn: 0,
+    totalInterestReturn: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+    const interestReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturn = totalAmount - investedAmount;
+    returnsArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturn,
+      month: timeReference,
+      totalAmount,
+    }) 
+  };
+  return returnsArray;
+};
+
